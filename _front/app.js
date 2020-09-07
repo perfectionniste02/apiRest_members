d720e39e01cb9fb54d16a9aad27d5aabcc07766e
Perfectionniste <yacouyacsonkeita@gmail.com>
2020-09-07 20:37:32 +0000

    yacou keita

--- _front/app.js
+++ _front/app.js
@@ -9,63 +9,103 @@ const { response } = require('express')
 
 //Variable globales
 const app = express()
-const port = 8080
+const port = 3000
 
 const fetch = axios.create({
-    baseURL: 'http://localhost:8081/api/v1',
-  });
+    baseURL: 'http://localhost:8081/api/v1'
+});
 
 // Middlewares
 app.use(morgan)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// page D'acceueil
-app.get('/' , (req , res) => {
-    res.render('index.twig' , {
-        name : req.params.name
-    })
+// page d'acceueil
+app.get('/', (req, res) => {
+    res.redirect('/members')
 })
 
 
 // Recuperation de tous les membres
-app.get('/members',(req , res) => {
-    fetch.get("/membres")
-        .then((response) => {
-            if (response.data.status == 'success') 
-            {
-                res.render('members.twig' , {
-                    members : response.data.result
-                })
-            }
-            else{renderError(res, response.data.message)}
+app.get('/members', (req, res) => {
+    apiCall(req.query.max ? `/membres?max= ${req.query.max}` : '/membres', 'get', {},  res, (result) => {
+        res.render('members.twig', {
+            members: result
         })
-        .catch((err) => renderError(res, err.message))
+    })
 })
 
 // recuperation d'un seul membre
 
-app.get('/members/:id', (req , res) => {
-    fetch.app(`/members/${req.params.name}`)
-    .then((response) => {
-        if (response.data.status == 'success') 
-            {
-                res.render('member.twig' , {
-                    members : response.data.result
-                })
-            }
-        else{renderError(res, response.data.message)}
+app.get('/members/:id', (req, res) => {
+    apiCall('/membres/' + req.params.id,'get', {}, res, (result) => {
+        res.render('member.twig', {
+            member: result
+        })
     })
-    .catch((err) => renderError(res , err.message))
+
+})
+
+// page gerant la modification d'un membree
+app.get('/edit/:id',(req, res) => {
+    apiCall('/membres/' + req.params.id,'get', {}, res, (result) => {
+        res.render('edit.twig', {
+            member: result
+        })
+    })
+})
+
+//methode permetant de modifier un membre
+app.post('/edit/:id',(req, res) => {
+    apiCall('/membres/' + req.params.id,'put', {name : req.body.name }, res, () => {
+        res.redirect('/members')
+    })
+})
+
+// methode permetant de supprimer un membre
+app.post('/delete' , (req , res) => {
+    apiCall('/membres/' + req.body.id, 'delete', {} ,res, () => {
+        res.redirect('/members')
+    })
+})
+
+//page gerant l'ajout d'un membre
+app.get('/insert' , (req , res) => {
+    res.render('insert.twig')
+})
+
+// methode permetant d'inserer un membre
+app.post('/insert' , (req , res) => {
+    apiCall('/membres', 'post', {name : req.body.name} ,res, () => {
+        res.redirect('/members')
+    })
 })
 
 // lancement de l'application
-app.listen(port,() => console.log(`started on port ${port}`))
+app.listen(port, () => console.log(`started on port ${port}`))
 
 // functions
 
-function renderError(res, err){
-    res.render('error.twig',{
-        errorMsg: err.message
+function renderError(res, err) {
+    res.render('error.twig', {
+        errorMsg: err
     })
 }
+
+function apiCall(url, method, data, res, next) {
+    fetch({
+        method : method,
+        url : url,
+        data : data
+    })
+    .then((response) => {
+            if (response.data.status == 'success') {
+                next(response.data.result)
+            }
+            else {
+                renderError(res, response.data.message)
+            }
+        })
+        .catch((err) => renderError(res, err.message))
+}
+
